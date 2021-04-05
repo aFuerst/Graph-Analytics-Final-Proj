6b915bae5603f8a5f1eea0527c44df6aea2064d8
alfuerst <alfuerst@iu.edu>
2021-04-05 17:21:46 -0400

    Add start of download script that we cam use to get all of our graphs consistently

---
+++ download.sh
@@ -0,0 +1,10 @@
+base="graphs"
+mkdir -p $base
+social="$base/social"
+mkdir -p $social
+
+# http://snap.stanford.edu/data/soc-LiveJournal1.html
+# wget  http://snap.stanford.edu/data/soc-LiveJournal1.txt.gz -O "$social/soc-LiveJournal1.txt.gz" 
+
+# http://snap.stanford.edu/data/soc-sign-bitcoin-otc.html
+wget https://snap.stanford.edu/data/soc-sign-bitcoinotc.csv.gz -O "$social/soc-sign-bitcoinotc.csv.gz" 
